2077d08cfa4f8c0d5193148ce126290047265165
Cleshz <minervashaji97@gmail.com>
2024-09-24 14:13:37 +0530

    shud be done one vercel side

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,292 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="text-red-200">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	//@ts-nocheck
+	import FaMoon from 'svelte-icons/fa/FaMoon.svelte';
+	import FaRegSun from 'svelte-icons/fa/FaRegSun.svelte';
+	import IoMdMenu from 'svelte-icons/io/IoMdMenu.svelte';
+	import TiHome from 'svelte-icons/ti/TiHome.svelte';
+	import GiArcheryTarget from 'svelte-icons/gi/GiArcheryTarget.svelte';
+	import FaRocket from 'svelte-icons/fa/FaRocket.svelte';
+	import IoIosFlower from 'svelte-icons/io/IoIosFlower.svelte';
+	import { Popover, Button } from 'flowbite-svelte';
+	import { slide } from 'svelte/transition';
+	import About from '$lib/About.svelte';
+	import Savishkaara from '$lib/Savishkaara.svelte';
+	import Team from '$lib/Team.svelte';
+	import ImageCarousel from '$lib/ImageCarousel.svelte';
+	import FaUsers from 'svelte-icons/fa/FaUsers.svelte';
+	import Contacts from '$lib/Contacts.svelte';
+	import IoMdCall from 'svelte-icons/io/IoMdCall.svelte';
+	import { navigating } from '$app/stores';
+
+	let placement: string = 'bottom';
+
+	let userTheme: boolean = localStorage.getItem('theme') === 'dark';
+	$: {
+		const htmlElement = document.documentElement;
+		if (userTheme) {
+			htmlElement.classList.add('dark');
+			localStorage.setItem('theme', 'dark');
+		} else {
+			htmlElement.classList.remove('dark');
+			localStorage.setItem('theme', 'light');
+		}
+	}
+
+	function toggleMenu() {
+		const menu = document.getElementById('mobile-menu');
+		// @ts-ignore
+		menu?.classList.toggle('hidden');
+	}
+</script>
+
+<body class="h-screen overflow-y-scroll dark:bg-neutral-900 dark:text-gray-300">
+	<nav class="sticky top-0 z-50 h-20 border-gray-200 bg-white dark:bg-neutral-900">
+		<div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4 pt-5">
+			<a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+				<img class="mt-2 h-14" src="/assets/img/asaslogo.png" alt="ASAS Logo" />
+			</a>
+
+			<button class="absolute right-5 mt-1 items-center hover:text-white md:hidden">
+				<Button id="placement-bottom">
+					<div class="h-12 text-gray-500 outline-none">
+						<IoMdMenu />
+					</div>
+				</Button>
+			</button>
+
+			<div class="hidden w-full md:block md:w-auto" id="navbar-default">
+				<ul
+					class="mt-4 flex flex-col rounded-lg border p-4 text-lg font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0 rtl:space-x-reverse"
+				>
+					<li>
+						<a
+							href="#Home"
+							class="block rounded bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 dark:text-white md:dark:text-blue-500"
+							aria-current="page">Home</a
+						>
+					</li>
+					<li>
+						<a href="#About" class="nav-link">About</a>
+					</li>
+					<li>
+						<a href="#Savishkaara" class="nav-link">Savishkaara</a>
+					</li>
+					<li>
+						<a href="#Team" class="nav-link">Team</a>
+					</li>
+					<li>
+						<a href="/events" class="nav-link">Events</a>
+					</li>
+					<li>
+						<button class="h-7" on:click={() => (userTheme = !userTheme)}>
+							<FaMoon />
+						</button>
+					</li>
+				</ul>
+			</div>
+			<button class="absolute right-24 h-7 md:hidden" on:click={() => (userTheme = !userTheme)}>
+				<FaMoon />
+			</button>
+		</div>
+	</nav>
+	<Popover
+		triggeredBy="#placement-bottom"
+		{placement}
+		transition={slide}
+		trigger="click"
+		class="z-20 h-80 w-full border-none bg-gray-100 opacity-70 shadow-lg shadow-black dark:bg-neutral-900"
+	>
+		<ul class="flex flex-col space-y-16 rounded-lg px-4 font-medium">
+			<li>
+				<a href="#Home" class="nav-mb"> <TiHome /><span class="ml-2 mt-2">Home</span></a>
+			</li>
+			<li>
+				<a href="#About" class="nav-mb"> <GiArcheryTarget /><span class="ml-2 mt-2">About</span></a>
+			</li>
+			<li>
+				<a href="#Savishkaara" class="nav-mb">
+					<IoIosFlower /><span class="ml-2 mt-1">Savishkaara</span></a
+				>
+			</li>
+			<li>
+				<a href="#Team" class="nav-mb"> <FaUsers /><span class="ml-2 mt-1">Team</span></a>
+			</li>
+			<!-- <li>
+				<a href="#Contacts" class="nav-mb"> <IoMdCall /><span class="ml-2 mt-1">Contacts</span></a>
+			</li> -->
+			<li>
+				<a href="/events" class="nav-mb pt-2"> <FaRocket /><span class="ml-4">Events</span></a>
+			</li>
+		</ul>
+	</Popover>
+
+	<section
+		id="Home"
+		class="sec -mt-24 grid h-screen grid-cols-3 items-center justify-center pb-20 pt-20 max-lg:grid-cols-none"
+	>
+		<div class="mx-10 max-lg:hidden">
+			<ImageCarousel />
+		</div>
+		<div class=" pb-5">
+			<img class="m-auto w-80" src="/assets/img/iedc_asas_2.png" alt="" loading="eager" />
+			<h1 class="text-center text-2xl font-bold md:text-4xl dark:text-violet-800">
+				INNOVATION AND<br />
+				ENTREPRENEURSHIP<br />
+				DEVELOPMENT CENTRE
+			</h1>
+			<h1 class="mt-5 text-center font-bold md:text-xl">
+				AMRITA VISHWA VIDHYAPEETHAM<br />KOCHI
+			</h1>
+			<div class="p-2 text-center">
+				<a href="https://innovate.startupmission.in/joinus/" target="_blank">
+					{#if userTheme}
+						<button class="btn btnDark">Join Today!</button>
+					{:else}
+						<button class="btn btnLight">Join Today!</button>
+					{/if}
+				</a>
+			</div>
+		</div>
+		<div class="mx-10 max-lg:hidden">
+			<ImageCarousel />
+		</div>
+	</section>
+
+	<section id="About" class="sec h-screen pt-20">
+		<About />
+	</section>
+
+	<section id="Savishkaara" class="sec h-screen pt-20">
+		{#key userTheme}
+		<Savishkaara/>
+		{/key}
+	</section>
+	<section id="Team" class="pt-20">
+		<Team />
+	</section>
+	<footer id="Contacts" class="sec h-screen pt-20">
+		<Contacts />
+	</footer>
+</body>
+
+<style>
+	footer {
+		width: 100%;
+		text-align: center;
+		padding: 1rem;
+	}
+
+	body,
+	nav {
+		@apply transition-colors duration-300;
+	}
+
+	.nav-link {
+		@apply block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500;
+	}
+
+	.nav-mb {
+		@apply absolute flex h-10 px-3 text-blue-700 dark:text-white;
+	}
+	/* From Uiverse.io by aidanwain */
+	.btn {
+		--border-color: linear-gradient(-45deg, #ffae00, #7e03aa, #00fffb);
+		--border-width: 0.125em;
+		--curve-size: 0.5em;
+		--blur: 30px;
+
+		color: var(--color);
+		cursor: pointer;
+		/* use position: relative; so that BG is only for .btn */
+		position: relative;
+		isolation: isolate;
+		display: inline-grid;
+		place-content: center;
+		padding: 0.5em 1.5em;
+		font-size: 17px;
+		border: 0;
+		text-transform: uppercase;
+		box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.6);
+		clip-path: polygon(
+			/* Top-left */ 0% var(--curve-size),
+			var(--curve-size) 0,
+			/* top-right */ 100% 0,
+			100% calc(100% - var(--curve-size)),
+			/* bottom-right 1 */ calc(100% - var(--curve-size)) 100%,
+			/* bottom-right 2 */ 0 100%
+		);
+		transition: color 250ms;
+	}
+	.btnDark {
+		--bg: #080312;
+		--color: #afffff;
+	}
+	.btnLight {
+		--bg: #fff;
+		--color: #080312;
+	}
+
+	.btn::after,
+	.btn::before {
+		content: '';
+		position: absolute;
+		inset: 0;
+	}
+
+	.btn::before {
+		background: var(--border-color);
+		background-size: 300% 300%;
+		animation: move-bg7234 5s ease infinite;
+		z-index: -2;
+	}
+
+	@keyframes move-bg7234 {
+		0% {
+			background-position: 31% 0%;
+		}
+
+		50% {
+			background-position: 70% 100%;
+		}
+
+		100% {
+			background-position: 31% 0%;
+		}
+	}
+
+	.btn::after {
+		background: var(--bg);
+		z-index: -1;
+		clip-path: polygon(
+			/* Top-left */ var(--border-width) calc(var(--curve-size) + var(--border-width) * 0.5),
+			calc(var(--curve-size) + var(--border-width) * 0.5) var(--border-width),
+			/* top-right */ calc(100% - var(--border-width)) var(--border-width),
+			calc(100% - var(--border-width))
+				calc(100% - calc(var(--curve-size) + var(--border-width) * 0.5)),
+			/* bottom-right 1 */ calc(100% - calc(var(--curve-size) + var(--border-width) * 0.5))
+				calc(100% - var(--border-width)),
+			/* bottom-right 2 */ var(--border-width) calc(100% - var(--border-width))
+		);
+		transition: clip-path 500ms;
+	}
+
+	.btn:where(:hover, :focus)::after {
+		clip-path: polygon(
+			/* Top-left */ calc(100% - var(--border-width))
+				calc(100% - calc(var(--curve-size) + var(--border-width) * 0.5)),
+			calc(100% - var(--border-width)) var(--border-width),
+			/* top-right */ calc(100% - var(--border-width)) var(--border-width),
+			calc(100% - var(--border-width))
+				calc(100% - calc(var(--curve-size) + var(--border-width) * 0.5)),
+			/* bottom-right 1 */ calc(100% - calc(var(--curve-size) + var(--border-width) * 0.5))
+				calc(100% - var(--border-width)),
+			/* bottom-right 2 */ calc(100% - calc(var(--curve-size) + var(--border-width) * 0.5))
+				calc(100% - var(--border-width))
+		);
+		transition: 200ms;
+	}
+
+	.btn:where(:hover, :focus) {
+		color: #fff;
+	}
+</style>
---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,291 @@
+<script lang="ts">
+	//@ts-nocheck
+	import FaMoon from 'svelte-icons/fa/FaMoon.svelte';
+	import TiArrowBack from 'svelte-icons/ti/TiArrowBack.svelte';
+	import EventCard from '$lib/EventCard.svelte';
+	import { Modals } from 'svelte-modals';
+	import { openModal } from 'svelte-modals';
+	import EventModal from '$lib/EventModal.svelte';
+	let userTheme: boolean = localStorage.getItem('theme') === 'dark';
+	$: {
+		const htmlElement = document.documentElement;
+		if (userTheme) {
+			htmlElement.classList.add('dark');
+			localStorage.setItem('theme', 'dark');
+		} else {
+			htmlElement.classList.remove('dark');
+			localStorage.setItem('theme', 'light');
+		}
+	}
+</script>
+
+<body
+	class="h-screen snap-y snap-mandatory overflow-y-scroll dark:bg-neutral-900 dark:text-gray-300"
+>
+	<nav class="sticky top-0 z-40 h-20 border-gray-200 bg-white dark:bg-neutral-900">
+		<div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4 pt-5">
+			<a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+				<!-- <img
+					src="/assets/img/main-icon.png"
+					class="h-10 rounded-full border border-neutral-900"
+					alt="IEDC Logo"
+				/> -->
+				<img class=" h-14" src="/assets/img/asaslogo.png" alt="ASAS Logo" />
+			</a>
+
+			<a class="absolute right-5 items-center hover:text-white md:hidden" href="/"
+				><div
+					class="h-12
+    text-gray-500 outline-none"
+				>
+					<TiArrowBack />
+				</div>
+			</a>
+
+			<div class="hidden w-full md:block md:w-auto" id="navbar-default">
+				<ul
+					class="mt-4 flex flex-col rounded-lg border p-4 text-lg font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0 rtl:space-x-reverse"
+				>
+					<li>
+						<a
+							href="/#Home"
+							class="block rounded bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 dark:text-white"
+							aria-current="page">Home</a
+						>
+					</li>
+					<li>
+						<a href="/#About" class="nav-link">About</a>
+					</li>
+					<li>
+						<a href="/#Savishkaara" class="nav-link">Savishkaara</a>
+					</li>
+					<li>
+						<a href="/#Team" class="nav-link">Team</a>
+					</li>
+					<li>
+						<a href="/events" class="nav-link">Events</a>
+					</li>
+					<li>
+						<button class="h-7" on:click={() => (userTheme = !userTheme)}>
+							<FaMoon />
+						</button>
+					</li>
+				</ul>
+			</div>
+			<button class="absolute right-24 h-7 md:hidden" on:click={() => (userTheme = !userTheme)}>
+				<FaMoon />
+			</button>
+		</div>
+	</nav>
+	<h1 class="text-center text-4xl font-bold text-pink-700 underline decoration-2 dark:text-red-700">
+		PAST EVENTS
+	</h1>
+	<div class="mb-40 mt-10 flex flex-wrap justify-center gap-6 pb-6">
+		<button
+			on:click={() =>
+				openModal(EventModal, {
+					title: 'AI Reinforced Deep Learning Workshop',
+					img: '/assets/Posters/deeplearning.jpg',
+					details:
+						'🏆 We’re thrilled to announce the AI Reinforced Deep Learning Workshop, a hands-on, full-day event on 24th September, starting at 9:30 AM in Lab 6, A Block!',
+					indepth:
+						'Have fun while learning AI! This workshop will give you the chance to explore cutting-edge AI techniques and apply them in a fun and engaging project. If you’ve ever wanted to delve into AI and deep learning in an exciting way, this is the workshop for you!',
+
+					highlights:
+						'Learn AI through a fun and interactive project.Get hands-on experience in real-world AI applications.Enhance your Python and AI skills.',
+					requirements:
+						'Open to All students.Only 60 seats available.Basic knowledge of Python is required.',
+					fee: '₹100',
+					contacts: 'Vaishnav - +91 6238996266. Hrishikesh Nair - +91 7510752549.'
+				})}
+		>
+			<EventCard
+				name="AI Reinforced Deep Learning Workshop"
+				url="/assets/Posters/deeplearning.jpg"
+				details="🏆 We’re thrilled to announce the AI Reinforced Deep Learning Workshop, a hands-on, full-day event on 24th September, starting at 9:30 AM in Lab 6, A Block!"
+			/>
+		</button>
+		<EventCard
+			name="Entrepreneurship And Financial Independence"
+			url="/assets/Posters/Entrepreneurship.jpg"
+			details="🏆 Todo"
+		/>
+		<EventCard
+			name="Ethical Hacking Bootcamp"
+			url="/assets/Posters/EthicalHacking.jpeg"
+			details="🏆 Todo"
+		/>
+		<EventCard
+			name="Generative AI - Introduction"
+			url="/assets/Posters/GenerativeAI.jpg"
+			details="🏆 Todo"
+		/>
+		<EventCard
+			name="InspireX"
+			url="/assets/Posters/InspireX.jpg"
+			details="🏆 Todo"
+		/>
+		<EventCard
+			name="Programming with GPU"
+			url="/assets/Posters/ProgrammingGpu.jpg"
+			details="🏆 Todo"
+		/>
+		<EventCard
+			name="Minor Project Orientation"
+			url="/assets/Posters/ProjectOrientation.jpg"
+			details="🏆 Todo"
+		/>
+		<EventCard
+			name="Research Methodology"
+			url="/assets/Posters/ResearchMethodology.jpg"
+			details="🏆 Todo"
+		/>
+		<EventCard
+			name="Securing Systems"
+			url="/assets/Posters/SecuringSystems.jpg"
+			details="🏆 Todo"
+		/>
+	</div>
+	<Modals></Modals>
+	<!-- 
+	<h1 class="text-center text-4xl font-bold text-pink-700 underline decoration-2 dark:text-red-700">
+		PAST EVENTS
+	</h1>
+	<div class="mb-40 mt-10 flex flex-wrap justify-center gap-6 pb-6">
+		<a href="/events/Entrepreneurship">
+			<EventCard
+				name="Entrepreneurship & Financial Independence"
+				url="/assets/dynamic/Entrepreneurship.jpg"
+				details="🏆 Todo"
+			/>
+		</a>
+
+		<a href="/events/EthicalHacking">
+			<EventCard
+				name="Ethical Hacking Bootcamp"
+				url="/assets/dynamic/EthicalHacking.jpg"
+				details="🏆 Todo"
+			/>
+		</a>
+
+		<a href="/events/GenerativeAI">
+			<EventCard name="Generative AI" url="/assets/dynamic/GenerativeAI.jpg" details="🏆 Todo" />
+		</a>
+
+		<a href="/events/IEEE">
+			<EventCard name="Generative AI" url="/assets/dynamic/IEEE.jpg" details="🏆 Todo" />
+		</a>
+
+		<a href="/events/InspireX">
+			<EventCard name="InspireX" url="/assets/dynamic/InspireX.jpg" details="🏆 Todo" />
+		</a>
+
+		<a href="/events/ProgrammingGpu">
+			<EventCard
+				name="Programming with GPU"
+				url="/assets/dynamic/ProgrammingGpu.jpg"
+				details="🏆 Todo"
+			/>
+		</a>
+
+		<a href="/events/ProjectOrientation">
+			<EventCard
+				name="Minor Project Orientation"
+				url="/assets/dynamic/ProjectOrientation.jpg"
+				details="🏆 Todo"
+			/>
+		</a>
+
+		<a href="/events/ResearchMethodology">
+			<EventCard
+				name="Research Methodology"
+				url="/assets/dynamic/ResearchMethodology.jpg"
+				details="🏆 Todo"
+			/>
+		</a>
+
+		<a href="/events/SecuringSystems">
+			<EventCard
+				name="Securing Systems"
+				url="/assets/dynamic/SecuringSystems.jpg"
+				details="🏆 Todo"
+			/>
+		</a>
+	</div> -->
+
+	<h1 class="text-center text-4xl font-bold text-pink-700 underline decoration-2 dark:text-red-700">
+		SAVISHKAARA 2024
+	</h1>
+	<div class="mt-10 flex flex-wrap justify-center gap-6 pb-6">
+		<EventCard
+			name="Amrita CTF"
+			url="/assets/Posters/amritactf.jpg"
+			details="🏆 Get ready to unleash your inner hacker in our adrenaline-fueled Capture the Flag competition, Hosted by the Department of Computer Science & IT. Dive into a world of cyber challenges, where every byte counts and victory awaits the bold. Register now and join us in the battle for digital supremacy!"
+		/>
+		<EventCard
+			name="Biz Blitz"
+			url="/assets/Posters/bizblitz.jpg"
+			details="🎖️Think outside the box and inside the market! Join us for the Department of Computer Science & IT's Biz Blitz, where participants unleash their creativity to market a range of products like never before. From innovative campaigns to captivating strategies, show us your marketing magic and seize the spotlight!"
+		/>
+		<EventCard
+			name="E-Football"
+			url="/assets/Posters/efootball.jpg"
+			details="🎖️Get ready to unleash your inner athlete in our electrifying e-football tournament, Hosted by the Department of Computer Science & IT. Dive into the digital arena as you compete in thrilling matches, showcasing your skills and strategic prowess. Every goal counts as you race towards victory and claim your place among the champions. Register now and join us in the journey towards e-football mastery."
+		/>
+		<EventCard
+			name="Electro Forge"
+			url="/assets/Posters/electroforge.jpg"
+			details="💡Calling all students from every department! Are you ready to unleash your inner hardware wizard and forge something extraordinary? Join us for Electro Forge, an electrifying hardware assembly competition proudly brought to you by the Department of Computer Science and IT. Get ready to roll up your sleeves, dive into the world of circuits and components, and let your creativity run wild !"
+		/>
+		<EventCard
+			name="Enigma"
+			url="/assets/Posters/enigma.jpg"
+			details="🏆 Gear up for a thrilling Logo Guess challenge, Hosted by the Department of Computer Science & IT. Dive into the world of branding and design as you decipher iconic logos. Every guess counts as you race towards victory in this ultimate test of visual recognition. Register now and join us in the battle for logo mastery!"
+		/>
+		<EventCard
+			name="Logo Design"
+			url="/assets/Posters/logodesign.jpg"
+			details="🏆 Get ready to unleash your creativity in our exhilarating Logo Design challenge, Hosted by the Department of Computer Science & IT. Dive into the world of branding and design as you craft iconic logos from scratch. Every stroke of your digital pen counts as you race towards the pinnacle of design excellence. Register now and join us in the journey towards logo mastery!"
+		/>
+		<EventCard
+			name="NerdScript 2.0"
+			url="/assets/Posters/nerdscript.jpg"
+			details="🧑‍💻Calling all code wizards! The Department of Computer Science & IT presents a thrilling competition where creativity meets challenge. Dive into the world of coding, solve complex problems, and emerge as the coding maestro!"
+		/>
+		<EventCard
+			name="Photo Scavenger"
+			url="/assets/Posters/photoscav.jpg"
+			details="🏆 Get ready to unleash your inner explorer in our exhilarating Photo Scavenger Hunt adventure, Hosted by the Department of Computer Science & IT. Dive into the world of photography as you capture moments and stories through your lens. Every snap counts as you race towards uncovering hidden gems and showcasing your creative prowess. Register now and join us in the journey towards photographic mastery!"
+		/>
+		<EventCard
+			name="Swiftknot Sprint"
+			url="/assets/Posters/swiftknotsprint.jpg"
+			details="🧑‍💻 Race against the clock and your peers in the ultimate keyboard showdown! Join us for the *Department of Computer Science & IT*'s Swiftknot Sprint and let your fingers do the talking. Who will emerge as the fastest typer?"
+		/>
+		<EventCard
+			name="Treasure Hunt"
+			url="/assets/Posters/treasurehunt.jpg"
+			details="🎖️ Embark on a quest for hidden treasures in our thrilling Treasure Hunt event, hosted by the *Department of Computer Science & IT*. Challenge your wits, solve mind-bending puzzles, and conquer exciting tasks in this ultimate adventure. Unleash your inner detective and race against time to uncover mysteries waiting to be solved!"
+		/>
+		<EventCard
+			name="Web Hackathon"
+			url="/assets/Posters/webhackathon.jpg"
+			details="🌐 Join the Department of Computer Science & IT's Web Hackathon for an exhilarating journey! Unleash your creativity as you develop web or mobile apps with database connectivity. With a themed challenge that sparks innovation, this event is your chance to showcase your skills and craft solutions that make a difference. Are you ready to code your way to success?"
+		/>
+		<EventCard
+			name="Xonotic"
+			url="/assets/Posters/xonotic.jpg"
+			details="🎖️ Prepare to enter the virtual arena and battle for supremacy in the *Department of Computer Science & IT*'s Xonotic event! Whether you're a seasoned player or a newcomer, this is your chance to showcase your skills and claim victory !"
+		/>
+	</div>
+</body>
+
+<style>
+	body,
+	nav {
+		@apply transition-colors duration-300;
+	}
+	.nav-link {
+		@apply block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500;
+	}
+</style>
