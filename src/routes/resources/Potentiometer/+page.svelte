b26baabdf670c346a1cfb3651d65434ab2e68395
Cleshz <minervashaji97@gmail.com>
2025-03-28 01:25:50 +0530

    Potentiometer done

---
+++ src/routes/resources/Potentiometer/+page.svelte
@@ -0,0 +1,180 @@
+<script lang="ts">
+	import MonacoEditor from '$lib/MonacoEditor.svelte';
+	import Navbar from '$lib/Navbar.svelte';
+	let code = `
+            
+
+#define POTENTIOMETER_PIN  36  // ESP32 pin GPIO36 (ADC0) connected to Potentiometer pin
+#define LED_PIN            21  // ESP32 pin GPIO21 connected to LED's pin
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+
+  // set the ADC attenuation to 11 dB (up to ~3.3V input)
+  analogSetAttenuation(ADC_11db);
+
+  // declare LED pin to be an output:
+  pinMode(LED_PIN, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // reads the input on analog pin A0 (value between 0 and 4095)
+  int analogValue = analogRead(POTENTIOMETER_PIN);
+
+  // scales it to brightness (value between 0 and 255)
+  int brightness = map(analogValue, 0, 4095, 0, 255);
+
+  // sets the brightness LED that connects to  pin 3
+  analogWrite(LED_PIN, brightness);
+
+  // print out the value
+  Serial.print("Analog value = ");
+  Serial.print(analogValue);
+  Serial.print(" => brightness = ");
+  Serial.println(brightness);
+  delay(100);
+}`;
+</script>
+
+<body class="h-screen overflow-y-scroll bg-white dark:bg-neutral-900 dark:text-gray-300">
+	<Navbar root="/resources" />
+
+	<section class="mx-auto max-w-3xl p-6">
+		<h1
+			class="mb-6 text-center text-4xl font-bold text-pink-700 underline decoration-2 dark:text-red-700"
+		>
+			Potentiometer LED Fade
+		</h1>
+
+		<div class="space-y-8">
+			<!-- Introduction -->
+			<div>
+				<h2 class="mb-2 text-2xl font-semibold text-gray-800 dark:text-white">Overview</h2>
+				<p class="text-base leading-relaxed">
+					Change the brightness of a LED using ESP32 and Potentiometer
+				</p>
+			</div>
+
+			<!-- Components Required -->
+			<div>
+				<h2 class="mb-2 text-2xl font-semibold text-gray-800 dark:text-white">
+					Components Required
+				</h2>
+				<ul class="list-disc space-y-2 pl-5">
+					<li>ESP32 Development Board (Any variant)</li>
+					<li>Micro USB Cable (Data-enabled)</li>
+					<li>Breadboard</li>
+					<li>LED (5mm preferred)</li>
+					<li>220Ω Resistor</li>
+					<li>Potentiometer</li>
+					<li>Jumper Wires</li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<section class="mx-auto max-w-4xl p-6">
+		<h1
+			class="mb-6 text-center text-4xl font-bold text-pink-700 underline decoration-2 dark:text-red-700"
+		>
+			ESP 32 Pinout
+		</h1>
+		<img class="md:w-full" src="/assets/img/resources/esp_pinout.png" alt="esppinout" />
+
+		<!-- <h1
+			class="mt-20 text-center text-4xl font-bold text-pink-700 underline decoration-2 dark:text-red-700"
+		>
+			LED Pinout
+		</h1>
+		<img class="scale-90" src="/assets/img/resources/led.png" alt="Ledpinout" /> -->
+
+		<!-- <h1
+			class="mt-20 text-center text-4xl font-bold text-pink-700 underline decoration-2 dark:text-red-700"
+		>
+			BreadBoard Layout
+		</h1>
+		<img class="scale-90" src="/assets/img/resources/breadboard.png" alt="Ledpinout" /> -->
+		<h1
+			class="mt-20 text-center text-4xl font-bold text-pink-700 underline decoration-2 dark:text-red-700"
+		>
+			Potentiometer Pinout
+		</h1>
+		<img class="scale-90" src="/assets/img/resources/pot.png" alt="Ledpinout" />
+
+		<h1
+			class="-mb-10 mt-20 text-center text-4xl font-bold text-pink-700 underline decoration-2 dark:text-red-700"
+		>
+			Circuit
+		</h1>
+		<img class="scale-75" src="/assets/img/resources/potentiometer_led.png" alt="pinout" />
+	</section>
+	<section class="mx-auto max-w-4xl p-6">
+		<h1
+			class="mb-6 mt-20 text-center text-4xl font-bold text-pink-700 underline decoration-2 dark:text-red-700"
+		>
+			Example Code
+		</h1>
+		<MonacoEditor {code} />
+		<div class="mt-32 flex justify-between">
+			<a href="/resources/LedToggle">
+				<button
+					class="group relative h-14 w-48 rounded-2xl border border-black bg-white text-center text-xl font-semibold text-black dark:bg-slate-700 dark:text-white"
+					type="button"
+				>
+					<div
+						class="absolute left-1 top-[4px] z-10 flex h-12 w-1/4 items-center justify-center rounded-xl bg-red-400 duration-500 group-hover:w-[184px]"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 1024 1024"
+							height="25px"
+							width="25px"
+						>
+							<path d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z" fill="#000000"></path>
+							<path
+								d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
+								fill="#000000"
+							></path>
+						</svg>
+					</div>
+					<p class="translate-x-2">Go Back</p>
+				</button>
+			</a>
+			<a href="/resources/WebServer">
+				<button
+					class="group relative h-14 w-48 rounded-2xl border border-black bg-white text-center text-xl font-semibold text-black dark:bg-slate-700 dark:text-white"
+					type="button"
+				>
+					<div
+						class="absolute right-1 top-[4px] z-10 flex h-12 w-1/4 rotate-180 items-center justify-center rounded-xl bg-green-400 duration-500 group-hover:w-[184px]"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 1024 1024"
+							height="25px"
+							width="25px"
+						>
+							<path d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z" fill="#000000"></path>
+							<path
+								d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
+								fill="#000000"
+							></path>
+						</svg>
+					</div>
+					<p class="-ml-3">Next</p>
+				</button>
+			</a>
+		</div>
+	</section>
+</body>
+
+<style>
+	body {
+		@apply transition-colors duration-500;
+	}
+	.nav-link {
+		@apply block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500;
+	}
+</style>
